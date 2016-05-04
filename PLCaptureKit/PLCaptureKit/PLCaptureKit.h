29a3416efd8894a59a95c630ca08f35ac56e1912
hzwangsiyu <hzwangsiyu@163.com>
2016-05-04 20:37:01 +0800

    initial capture kit

---
+++ PLCaptureKit/PLCaptureKit/PLCaptureKit.h
@@ -0,0 +1,18 @@
+//
+//  PLCaptureKit.h
+//  PLCaptureKit
+//
+//  Created by WangSiyu on 5/3/16.
+//  Copyright © 2016 Pili Engineering, Qiniu Inc. All rights reserved.
+//
+
+#ifndef PLCaptureKit_h
+#define PLCaptureKit_h
+
+#import "PLAACTypeDefines.h"
+#import "PLCameraSource.h"
+#import "PLCameraStreamingSession.h"
+#import "PLMicrophoneSource.h"
+#import "PLSourceAccessProtocol.h"
+
+#endif /* PLCaptureKit_h */
